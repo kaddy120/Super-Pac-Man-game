3d52d04e44b1614700d01d3a30037190265aeaf3
kaddy120 <kaddy120@gmail.com>
2020-08-23 18:52:28 +0200

    new class and few modification

--- game-source-code/Ghost.h
+++ game-source-code/Ghost.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "Sprite.h"
+#include "string"
+
+enum class Mode { Chase, Eatable, InTheBox, Captured };
 
 class Ghost : public Sprite
 {
+private: 
+	std::string Colour;
+	Mode Mode_;
+
 };
 
--- game-source-code/PacMan.h
+++ game-source-code/PacMan.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Sprite.h"
 
+enum class Mode {Eatable, charged, SuperCharged, Dead };
+
+enum class Direction { Up, Down, Right, Left };
+
 class PacMan: public Sprite
 {
 
