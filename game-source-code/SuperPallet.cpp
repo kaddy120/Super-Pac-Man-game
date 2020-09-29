39b69f10a0d8fd9d41d05cd6fc09f31ceb080fd6
dembe122 <edgar.munyai18@gmail.com>
2020-09-30 00:08:27 +0200

    Superpellet added

---
+++ game-source-code/SuperPallet.cpp
@@ -0,0 +1,15 @@
+#include "SuperPallet.h"
+
+SuperPallet::SuperPallet(
+	const float& radius, 
+	const Vector2& postion,
+	const unsigned int& points): CircleSprite(radius, postion), Points_(points)
+{}
+
+unsigned int SuperPallet::Points() const {
+	return Points_;
+}
+
+void SuperPallet::SetPoints(const unsigned int& points){
+	Points_ = points;
+}
